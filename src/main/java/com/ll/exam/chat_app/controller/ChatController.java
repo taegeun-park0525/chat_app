de420a17c96a71afb6b77a1d1bd5915b74cf1a7d
taegeun <djqtek0525@naver.com>
2022-12-19 10:15:39 +0900

    [add] Post /chat/writeMessage

---
+++ src/main/java/com/ll/exam/chat_app/controller/ChatController.java
@@ -0,0 +1,17 @@
+package com.ll.exam.chat_app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
+
+    @PostMapping("/writeMessage")
+    @ResponseBody
+    public String writeMessage() {
+        return "메세지가 작성되었습니다.";
+    }
+}
